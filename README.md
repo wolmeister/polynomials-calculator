f094e687069ecb2176c72250502d4cc91c1c85e0
Victor Wolmeister <victor.wolmeister@thomsonreuters.com>
2021-10-25 21:39:38 -0300

    Implement expression parser

---
+++ README.md
@@ -0,0 +1,21 @@
+# Polynomial calculator
+
+## Regexes playground
+
+```
+^[-+]?(\d+\.\d+|\d*)?
+```
+
+```
+2x
+x
+2.5x^2y^3
+4xy
+3
+2.5x
+2.5xy
+-5
+-6.5x
+-10xy^2
+x^11
+```
--- polynomial.py
+++ polynomial.py
@@ -7,63 +7,70 @@ class Monomial:
   indeterminates: dict[str, int]
   degree: int
 
-class PolynomialExpressionParser:
-  # This regex is used to validate a expression
-  _EXPRESSION_REGEX = r'^(([-+](?=[A-Za-z0-9]))?(\d+\.\d+|\d*)([A-Za-z](\^\d+)*)*)+$'
-  # This regex is used to extract all the terms (monomials) from a expression
-  _TERMS_REGEX = r'(?:(?:[-+])?(?:\d+\.\d+|\d*))(?:[A-Za-z](?:\^\d+)*)*'
-  # This regex is used to extract the monomial data from a term
-  _TERM_REGEX = r'([-+])?(\d+\.\d+|\d*)([A-Za-z])(?:\^(\d)+)'
+def parse_polynomial_expression(expression: str) -> list[Monomial]:
+  # Validates if the expression is valid
+  expression_regex = r'^(([-+](?=[A-Za-z0-9]))?(\d+\.\d+|\d*)([A-Za-z](\^\d+)*)*)+$'
+  if bool(re.match(expression_regex, expression)) == False:
+    raise Exception(f'Expression is not a valid polynomial: {expression}')
 
-  def parse(self, expression: str) -> list[Monomial]:
-    if bool(re.match(self._EXPRESSION_REGEX, expression)) == False:
-      raise Exception(f'Expression is not a valid polynomial: {expression}')
+  # Find all expression terms
+  terms_regex = r'(?:(?:[-+])?(?:\d+\.\d+|\d*))(?:[A-Za-z](?:\^\d+)*)*'
+  expression_terms = list(filter(len, re.findall(terms_regex, expression)))
 
-    expressionTerms = re.findall(self._TERMS_REGEX, expression)
+  # Parse expression terms in monomials objects
+  monomials: list[Monomial] = []
 
-    return list( map(self._parseExpressionTerm, filter(len, expressionTerms)))
-    # for expressionTerm in filter(len, expressionTerms):
-    #   print(expressionTerm)
-
-  def _parseExpressionTerm(self, expressionTerm: str) -> Monomial:
-    matches = re.findall(self._TERM_REGEX, expressionTerm)
-
-    for match in matches:
-      print(match)
-    
-    # group 0 = signal
-    # group 1 = coefficient
-    # group 2 = variable
-    # group 3 = degree
+  for term in expression_terms:
+    coefficient = 1;
+    degree = 0;
     indeterminates: dict[str, int] = {}
- 
-    # print(expressionTerm)
-    # print(re.findall(self._TERM_REGEX, expressionTerm))
-    return Monomial(coefficient=0, indeterminates={}, degree=0)
+
+    # Parse coefficient
+    coefficient_regex = r'^[-+]?(\d+\.\d+|\d*)?'
+    coefficient_string = re.search(coefficient_regex, term)[0].replace('+', '')
+
+    if len(coefficient_string):
+      if coefficient_string == '-':
+        coefficient = -1;
+      else:
+        coefficient = float(coefficient_string)
+
+    # Parse indeterminates
+    indeterminates_regex = r'([A-Za-z])(?:\^(\d+))?'
+
+    for indeterminate_match in re.findall(indeterminates_regex, term):
+      indeterminate_degree = 1;
+      if len(indeterminate_match[1]):
+        indeterminate_degree = int(indeterminate_match[1])
+
+      indeterminates[indeterminate_match[0]] = indeterminate_degree
+      degree += indeterminate_degree
+
+    monomials.append(Monomial(coefficient=coefficient, indeterminates=indeterminates, degree=degree))
+
+  return monomials
 
 
 class Polynomial:
   _terms: list[Monomial]
 
   def __init__(self, expression: str) -> None:
-    self._terms = PolynomialExpressionParser().parse(expression=expression)
-    # matched = re.match(r"^(?:(-|\+)?((\d+\.\d+)|\d)*([A-Za-z](\^\d+)?)*)+$", expression)
-    # # matched = re.match(r"(-|\+)?((\d+\.\d+)|\d)*([A-Za-z](\^\d+)?)*", expression)
-    # is_match = bool(matched)
-    # print(is_match)
-    # pass
+    self._terms = parse_polynomial_expression(expression=expression)
 
   def __str__(self) -> str:
-    return 'nice'
+    return str(self._terms)
 
 
 if __name__ == '__main__':
-  # print('2x+2x ->', Polynomial('2x+2x'))
-  # print('2x-2x ->', Polynomial('2x-2x'))
-  # print('2+2x ->', Polynomial('2+2x'))
-  # print('2-2x ->', Polynomial('2-2x'))
-  # print('2xy-2 ->', Polynomial('2xy-2'))
-  # print('2.5x+4y ->', Polynomial('2.5x+4y'))
-  # print('2.5x^2 ->', Polynomial('2.5x^2'))
-  print('2.5x^2y^3-2x^3 ->', Polynomial('2.5x^2y^3-2x^3')._terms)
-  # print('2.5x^2y^3-2x^ ->', Polynomial('2.5x^2y^3-2x^'))
+  print('1 ->', Polynomial('1'))
+  print('x+y ->', Polynomial('x+y'))
+  print('x-y ->', Polynomial('x-y'))
+  print('2x+2x ->', Polynomial('2x+2x'))
+  print('2x-2x ->', Polynomial('2x-2x'))
+  print('2+2x ->', Polynomial('2+2x'))
+  print('2-2x ->', Polynomial('2-2x'))
+  print('2xy-2 ->', Polynomial('2xy-2'))
+  print('2.5x+4y ->', Polynomial('2.5x+4y'))
+  print('3x^2 ->', Polynomial('3x^2'))
+  print('2.5x^2 ->', Polynomial('2.5x^2'))
+  print('4.5x^2y^3-2x^3 ->', Polynomial('4.5x^2y^3-2x^3'))
