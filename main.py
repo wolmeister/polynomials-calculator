641b5a58a472ef85f35565cecca7bedc947315d1
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-11-29 19:47:56 -0300

    Remove test code from polynomial and move it to main

---
+++ main.py
@@ -0,0 +1,9 @@
+from polynomial import Polynomial
+
+if __name__ == '__main__':
+  print(Polynomial('4.5x^2y^3-2x^3+y^6').multiply(Polynomial('4.5x^2y^3-2x^3+y^6')))
+  print(Polynomial('y^6').multiply(Polynomial('y^6')))
+  print(Polynomial('x^2+x+1').multiply(Polynomial('y+1')))
+  print(Polynomial('x+y').multiply(Polynomial('y+x')))
+  print(Polynomial('3x+y').subtract(Polynomial('2x+y')))
+  print(Polynomial('x^2').sum(Polynomial('y+2x-2x^2')))
